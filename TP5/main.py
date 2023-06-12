e00dc52af55bfaace3c1d45756aac5e8b5ca3b79
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2023-06-12 19:53:14 -0300

    Comienzo TP5

--- TP5/main.py
+++ TP5/main.py
@@ -1,1 +1,17 @@
-# aca el programa tiene que hacer las cosas importantes por eso se llama main (: 
+# aca el programa tiene que hacer las cosas importantes por eso se llama main (:
+"""
+
+Tareas:
+
+- Expresion regular para MACs de APs (columna: MAC_AP) y largar lista de APs
+- Asignar nombre a cada AP 
+- Expresion regular para fechas (Columna: Inicio_de_conexio_dia y Fin_de_conexio_dia)
+- Expresion regular para usuarios (Columna: Usuario) y largar lista de usuarios
+- Vamos a usar el modulo Pandas para manipular el csv
+- Vamos a usar el modulo re para las expresiones regulares
+
+
+"""
+
+import re
+import pandas as pd
