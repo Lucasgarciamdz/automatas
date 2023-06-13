be8002b20b2096d64376cd2c9d097077889c7340
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2023-06-13 16:47:15 -0300

    REGEX + logica csv + testing REGEX

--- TP5/main.py
+++ TP5/main.py
@@ -10,8 +10,65 @@ Tareas:
 - Vamos a usar el modulo Pandas para manipular el csv
 - Vamos a usar el modulo re para las expresiones regulares
 
+13/06/2023:
+
+- Ya estan todas las REGEX (chequear que esten bien)
+- La logica y manejo del csv ya estaria (refactorizable igual)
+- Hice testing para las REGEX
 
 """
 
 import re
 import pandas as pd
+
+
+mac_regex = r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+date_regex = r'(20(1[0-9]|2[0-3])[-/](0[1-9]|1[0-2])[-/]([0-2][0-9]|3[0-1])$)'
+user_regex = r'([a-z]|[A-Z])*$'  
+
+# devuelve un DataFrame de todo el csv
+data = pd.read_csv("/home/franco/Escritorio/automatas-gramatica/export-2019-to-now-v4.csv", low_memory=False)
+
+# devuelve un DataFrame de las MACs de los APs
+ap_mac_list = data["MAC_AP"].unique()
+ap_mac_list_sorted = []
+for ap in ap_mac_list:
+    if re.fullmatch(mac_regex, ap):
+        with open("aps.txt", "a+") as file:
+            file.write(ap + "\n")
+
+# for mac in ap_mac_list_sorted:
+#     with open("macs.txt", "a+") as file:
+#         file.write(mac + "\n")
+
+# devuelve un DataFrame de los nombres de usuarios posibles
+user_list = data["Usuario"].unique()
+user_list_sorted = []
+for user in user_list:
+    if re.fullmatch(user_regex, user):
+        with open("users.txt", "a+") as file:
+            file.write(user + "\n")
+
+# for user in user_list_sorted:
+#     with open("users.txt", "a+") as file:
+#         file.write(user + "\n")
+
+# Imprime la totalidad de los APs
+# print("Lista APs")
+# print("--------------")
+# file = open("aps.txt", "r+")
+# print(file.read())
+# ap = input("Ingrese un AP: ")
+# date_start = input("Ingrese fecha de inicio: ")
+# date_end = input("Ingrese fecha de fin: ")
+
+ap = "14-96-E5-D1-BA-89"
+date_start = "2019-02-07"
+date_end = "2020-02-14"
+
+
+print(f'Usuarios conectados al AP {ap} entre {date_start} y {date_end}:')
+users = data.loc[data["MAC_AP"] == ap]
+users = users.loc[users["Inicio_de_Conexión_Dia"] > date_start]
+users = users.loc[users["FIN_de_Conexión_Dia"] < date_end]["Usuario"].unique()
+print(users)
---
+++ TP5/test.py
@@ -0,0 +1,35 @@
+import unittest
+import re
+from main import date_regex, mac_regex, user_regex
+
+class test_regex(unittest.TestCase):
+    
+    def test_date1(self):
+        self.assertTrue(re.fullmatch(date_regex, "2019-12-31"))
+    def test_date2(self):
+        self.assertIsNone(re.fullmatch(date_regex, "2000-12-01"))
+    def test_date3(self):
+        self.assertIsNone(re.fullmatch(date_regex, "2020-13-01"))
+    def test_date4(self):
+        self.assertIsNone(re.fullmatch(date_regex, "2020-12-32"))
+    def test_MAC(self):
+        self.assertTrue(re.fullmatch(mac_regex, "00:00:00:00:00:00"))
+    def test_MAC2(self):
+        self.assertTrue(re.fullmatch(mac_regex, "00-00-00-00-00-00"))
+    def test_MAC3(self):
+        self.assertIsNone(re.fullmatch(mac_regex, "0:00:00:00:0:00"))
+    def test_MAC4(self):
+        self.assertIsNone(re.fullmatch(mac_regex, "00:00:00:00:00:000"))
+    def test_MAC5(self):
+        self.assertIsNone(re.fullmatch(mac_regex, "\0:00:00:00:00:00"))
+    def test_user1(self):
+        self.assertTrue(re.fullmatch(user_regex, "pepe"))
+    def test_user2(self):
+        self.assertTrue(re.fullmatch(user_regex, "pEpE"))
+    def test_user4(self):
+        self.assertIsNone(re.fullmatch(user_regex, "pepe-honguito"))
+    def test_user5(self):
+        self.assertIsNone(re.fullmatch(user_regex, "pepe_honguito"))
+
+if __name__ == "__main__":
+    unittest.main()
