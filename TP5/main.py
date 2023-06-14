993ee73981226eaf85ebac06b1c71b5955c92c92
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2023-06-13 22:53:02 -0300

    version ~alienigena~

--- TP5/main.py
+++ TP5/main.py
@@ -22,53 +22,52 @@ import re
 import pandas as pd
 
 
-mac_regex = r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
-date_regex = r'(20(1[0-9]|2[0-3])[-/](0[1-9]|1[0-2])[-/]([0-2][0-9]|3[0-1])$)'
-user_regex = r'([a-z]|[A-Z])*$'  
+# Imprime la totalidad de los APs y pide que le pasemos uno junto con la fecha de inicio y fin de sesion
+def aps():
+    print("Lista APs")
+    print("--------------")
+    print(ap_mac_list)
+    ap = input("Ingrese un AP: ")
+    date_start = input("Ingrese fecha de inicio: ")
+    date_end = input("Ingrese fecha de fin: ")
+    return ap, date_start, date_end
 
-# devuelve un DataFrame de todo el csv
-data = pd.read_csv("/home/franco/Escritorio/automatas-gramatica/export-2019-to-now-v4.csv", low_memory=False)
+# Devulve los usuarios conectados dado un cierto AP, fecha de inicio de sesion y fecha de fin de sesion
+def obtain():
+    # ap, date_start, date_end = aps()
+    ap = "04-18-D6-22-38-E1:HCDD"
+    date_start = "2019-02-07"
+    date_end = "2020-02-14"
+    print(f'Usuarios conectados al AP {ap} entre {date_start} y {date_end}:')
+    users = data.loc[data["MAC_AP"] == ap ]
+    users = users.loc[users["Inicio_de_Conexión_Dia"] > date_start]
+    users = users.loc[users["FIN_de_Conexión_Dia"] < date_end]["Usuario"].unique()
+    print(users)
 
-# devuelve un DataFrame de las MACs de los APs
-ap_mac_list = data["MAC_AP"].unique()
-ap_mac_list_sorted = []
-for ap in ap_mac_list:
-    if re.fullmatch(mac_regex, ap):
-        with open("aps.txt", "a+") as file:
-            file.write(ap + "\n")
+if __name__ == '__main__':
 
-# for mac in ap_mac_list_sorted:
-#     with open("macs.txt", "a+") as file:
-#         file.write(mac + "\n")
+    # definimos las expresiones regulares
+    mac_regex = re.compile(r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}):HCDD$')
+    date_regex = re.compile(r'(20(1[0-9]|2[0-3])[-/](0[1-9]|1[0-2])[-/]([0-2][0-9]|3[0-1])$)')
+    user_regex = re.compile(r'([a-z]|[A-Z])*$')
 
-# devuelve un DataFrame de los nombres de usuarios posibles
-user_list = data["Usuario"].unique()
-user_list_sorted = []
-for user in user_list:
-    if re.fullmatch(user_regex, user):
-        with open("users.txt", "a+") as file:
-            file.write(user + "\n")
+    # devuelve un DataFrame de las columnas que vamos a ocupar del csv
+    data = pd.read_csv("/home/franco/Escritorio/automatas-gramatica/export-2019-to-now-v4.csv", 
+                    low_memory=False, 
+                    usecols=["MAC_AP", "Inicio_de_Conexión_Dia", "FIN_de_Conexión_Dia", "Usuario"])
 
-# for user in user_list_sorted:
-#     with open("users.txt", "a+") as file:
-#         file.write(user + "\n")
-
-# Imprime la totalidad de los APs
-# print("Lista APs")
-# print("--------------")
-# file = open("aps.txt", "r+")
-# print(file.read())
-# ap = input("Ingrese un AP: ")
-# date_start = input("Ingrese fecha de inicio: ")
-# date_end = input("Ingrese fecha de fin: ")
-
-ap = "14-96-E5-D1-BA-89"
-date_start = "2019-02-07"
-date_end = "2020-02-14"
+    # filtra los datos que no cumplen con las expresiones regulares
+    data = data[data["MAC_AP"].apply(lambda x: mac_regex.fullmatch(str(x)) is not None) &
+                        data["Inicio_de_Conexión_Dia"].apply(lambda x: date_regex.fullmatch(str(x)) is not None) &
+                        data["FIN_de_Conexión_Dia"].apply(lambda x: date_regex.fullmatch(str(x)) is not None) &
+                        data["Usuario"].apply(lambda x: user_regex.fullmatch(str(x)) is not None)]
 
 
-print(f'Usuarios conectados al AP {ap} entre {date_start} y {date_end}:')
-users = data.loc[data["MAC_AP"] == ap]
-users = users.loc[users["Inicio_de_Conexión_Dia"] > date_start]
-users = users.loc[users["FIN_de_Conexión_Dia"] < date_end]["Usuario"].unique()
-print(users)
+    # devuelve un DataFrame de las MACs de los APs
+    ap_mac_list = data["MAC_AP"].unique()
+
+    # devuelve un DataFrame de los nombres de usuarios posibles
+    user_list = data["Usuario"].unique()
+
+    obtain()
+
