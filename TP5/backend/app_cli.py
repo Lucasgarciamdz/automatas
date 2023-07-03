5cbc965b364b55e0692598a60206ab5b03f9fcb3
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2023-07-03 00:12:21 -0300

    listo para entrega sin documentacion

--- TP5/backend/app_cli.py
+++ TP5/backend/app_cli.py
@@ -1,106 +1,89 @@
 import typer
 import inquirer
 from yaspin import yaspin
-from csv_tool import create_pandas, apply_regex, DATE_RE
+from csv_tool import DATE_RE, apply_regex, create_pandas
 from tabulate import tabulate
 import pandas as pd
 
 
 def menu(csv_path: str = typer.Option(..., prompt="Ingrese la ruta del archivo csv")):
-    csv_read_spinner = yaspin(text="Leyendo csv", color="yellow")
-    apply_regex_spinner = yaspin(text="Aplicando expresiones regulares", color="yellow")
 
-    csv_read_spinner.start()
-    data = create_pandas(csv_path)
-    csv_read_spinner.stop()
+    INICIO_CONEXION_DIA = "Inicio_de_Conexión_Dia"
+    FIN_CONEXION_DIA = "FIN_de_Conexión_Dia"
 
-    apply_regex_spinner.start()
-    data = apply_regex(data)
-    apply_regex_spinner.stop()
+    with yaspin(text="Leyendo csv", color="yellow"):
+        data = create_pandas(csv_path)
+
+    with yaspin(text="Aplicando expresiones regulares", color="yellow"):
+        data = apply_regex(data)
 
     ap_list = data["MAC_AP"].unique().tolist()
 
-    ap = inquirer.prompt(
-        [inquirer.List("ACCESS POINT", message="Seleccione un AP", choices=ap_list)]
-    )
-
-    fechas = inquirer.prompt(
-        [
-            inquirer.Text(
-                "FECHA DE INICIO",
-                message="Ingrese la fecha de inicio (YYYY/MM/DD)",
-                validate=lambda _, x: DATE_RE.fullmatch(x) is not None,
-            ),
-            inquirer.Text(
-                "FECHA DE FIN",
-                message="Ingrese la fecha de fin (YYYY/MM/DD)",
-                validate=lambda _, x: DATE_RE.fullmatch(x) is not None,
-            ),
-        ]
-    )
-
-    ap, fecha_i, fecha_f = (
-        ap["ACCESS POINT"],
-        fechas["FECHA DE INICIO"],
-        fechas["FECHA DE FIN"],
-    )
-
-    fecha_i = pd.to_datetime(fecha_i)
-    fecha_f = pd.to_datetime(fecha_f)
-    data["Inicio_de_Conexión_Dia"] = pd.to_datetime(data["Inicio_de_Conexión_Dia"])  # Convertir la columna a tipo datetime
-    data["FIN_de_Conexión_Dia"] = pd.to_datetime(data["FIN_de_Conexión_Dia"])  # Convertir la columna a tipo datetime
-
-    entre_fechas = \
-        (data["Inicio_de_Conexión_Dia"].dt.year >= fecha_i.year)\
-        & (data["Inicio_de_Conexión_Dia"].dt.month >= fecha_i.month)\
-        & (data["Inicio_de_Conexión_Dia"].dt.day >= fecha_i.day)\
-        & (data["FIN_de_Conexión_Dia"].dt.year <= fecha_f.year)\
-        & (data["FIN_de_Conexión_Dia"].dt.month <= fecha_f.month)\
-        & (data["FIN_de_Conexión_Dia"].dt.day <= fecha_f.day)    
-    
-    anterior_fecha_i = \
-        (data["FIN_de_Conexión_Dia"].dt.year >= fecha_i.year)\
-        & (data["FIN_de_Conexión_Dia"].dt.month >= fecha_i.month)\
-        & (data["FIN_de_Conexión_Dia"].dt.day >= fecha_i.day)\
-        & (data["FIN_de_Conexión_Dia"].dt.year <= fecha_f.year)\
-        & (data["FIN_de_Conexión_Dia"].dt.month <= fecha_f.month)\
-        & (data["FIN_de_Conexión_Dia"].dt.day <= fecha_f.day)
-    
-    despues_fecha_f = \
-        (data["Inicio_de_Conexión_Dia"].dt.year >= fecha_i.year)\
-        & (data["Inicio_de_Conexión_Dia"].dt.month >= fecha_i.month)\
-        & (data["Inicio_de_Conexión_Dia"].dt.day >= fecha_i.day)\
-        & (data["Inicio_de_Conexión_Dia"].dt.year <= fecha_f.year)\
-        & (data["Inicio_de_Conexión_Dia"].dt.month <= fecha_f.month)\
-        & (data["Inicio_de_Conexión_Dia"].dt.day <= fecha_f.day)
-    
-    fuera_fechas = \
-        (data["Inicio_de_Conexión_Dia"].dt.year <= fecha_i.year)\
-        & (data["Inicio_de_Conexión_Dia"].dt.month <= fecha_i.month)\
-        & (data["Inicio_de_Conexión_Dia"].dt.day <= fecha_i.day)\
-        & (data["FIN_de_Conexión_Dia"].dt.year >= fecha_f.year)\
-        & (data["FIN_de_Conexión_Dia"].dt.month >= fecha_f.month)\
-        & (data["FIN_de_Conexión_Dia"].dt.day >= fecha_f.day)
-    
-    filtro = ((data["MAC_AP"] == ap) & ((entre_fechas) | (anterior_fecha_i) | (despues_fecha_f) | (fuera_fechas)))
-
-    data = data[filtro]
-    users = data["Usuario"].unique()
-
-    print("")
-    print(
-        tabulate(
-            [[user] for user in users],
-            headers=[
-                f'Usuarios conectados al AP {ap} entre {fecha_i} y {fecha_f}'
-            ],
-            tablefmt="grid",
-            stralign="center",
+    while True:
+        ap = inquirer.prompt(
+            [inquirer.List("ACCESS POINT", message="Seleccione un AP", choices=ap_list)]
         )
-    )
 
-    with open("output.csv", "w+") as f:
-        f.write(data.to_csv(index=False))
+        fechas = inquirer.prompt(
+            [
+                inquirer.Text(
+                    "FECHA DE INICIO",
+                    message="Ingrese la fecha de inicio (YYYY-MM-DD)",
+                    validate=lambda _, x: DATE_RE.fullmatch(x) is not None,
+                ),
+                inquirer.Text(
+                    "FECHA DE FIN",
+                    message="Ingrese la fecha de fin (YYYY-MM-DD)",
+                    validate=lambda _, x: DATE_RE.fullmatch(x) is not None,
+                ),
+            ]
+        )
+
+        ap, fecha_i, fecha_f = (
+            ap["ACCESS POINT"],
+            pd.to_datetime(fechas["FECHA DE INICIO"]),
+            pd.to_datetime(fechas["FECHA DE FIN"]),
+        )
+
+        data[INICIO_CONEXION_DIA] = pd.to_datetime(data[INICIO_CONEXION_DIA])
+        data[FIN_CONEXION_DIA] = pd.to_datetime(data[FIN_CONEXION_DIA])
+
+        filtro = (
+            (data["MAC_AP"] == ap) &
+            (
+                (data[INICIO_CONEXION_DIA].between(fecha_i, fecha_f)) |
+                (data[FIN_CONEXION_DIA].between(fecha_i, fecha_f)) |
+                (data[INICIO_CONEXION_DIA] <= fecha_i) & (data[FIN_CONEXION_DIA] >= fecha_f)
+            )
+        )
+
+        data = data.loc[filtro]
+        users = data["Usuario"].unique()
+
+        consulta = f'Usuarios conectados al AP {ap} entre {fecha_i} y {fecha_f}'
+        print("")
+        print(
+            tabulate(
+                [[user] for user in users],
+                headers=[
+                    consulta
+                ],
+                tablefmt="grid",
+                stralign="center",
+            )
+        )
+
+        if typer.confirm("¿Desea guardar los datos en un archivo csv?"):
+            write_to_csv(data, consulta)
+
+        if not typer.confirm("¿Desea realizar otra consulta?"):
+            print("Gracias por usar nuestro programa")
+            break
+
+
+def write_to_csv(data: pd.DataFrame, consulta: str):
+    file_name = f'output_{consulta}.csv'
+    data.to_csv(file_name, index=False)
 
 
 if __name__ == "__main__":
--- TP5/backend/csv_tool.py
+++ TP5/backend/csv_tool.py
@@ -3,44 +3,24 @@ import re
 
 # Expresiones regulares
 MAC_RE = re.compile(r"([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}):HCDD$")
-DATE_RE = re.compile(r"(20(1[5-9]|2[0-5])[-/](0[1-9]|1[0-2])[-/]([0-2][0-9]|3[0-1])$)")
+DATE_RE = re.compile(r"(20(1[5-9]|2[0-5])[-/](0[1-9]|1[0-2])[-/]([0-2]\d|3[0-1])$)")
 USER_RE = re.compile(r"[a-zA-Z0-9-_\\/]{1,25}$")
 
 
-def create_pandas(
-    csv_path: str,
-    columns: list = [
-        "MAC_AP",
-        "Inicio_de_Conexión_Dia",
-        "FIN_de_Conexión_Dia",
-        "Usuario",
-    ],
-):
+def create_pandas(csv_path: str, columns: list = ["MAC_AP", "Inicio_de_Conexión_Dia", "FIN_de_Conexión_Dia", "Usuario"]) -> pd.DataFrame:
+
     data = pd.read_csv(csv_path, low_memory=False, usecols=columns)
 
     return data
 
 
-def apply_regex(data: pd.DataFrame):
-    mask = (
-        data["MAC_AP"].str.fullmatch(MAC_RE)
-        & data["Inicio_de_Conexión_Dia"].str.fullmatch(DATE_RE)
-        & data["FIN_de_Conexión_Dia"].str.fullmatch(DATE_RE)
-        & data["Usuario"].str.fullmatch(USER_RE)
+def apply_regex(data: pd.DataFrame) -> pd.DataFrame:
+    filtro = (
+        data["MAC_AP"].str.match(MAC_RE)
+        & data["Inicio_de_Conexión_Dia"].str.match(DATE_RE)
+        & data["FIN_de_Conexión_Dia"].str.match(DATE_RE)
+        & data["Usuario"].str.match(USER_RE)
     )
-    data = data[mask]
+    data = data[filtro]
 
     return data
-
-
-# def get_ap_list(data: pd.DataFrame):
-#     return data["MAC_AP"].unique()
-
-
-# if __name__ == '__main__':
-#     start = time.time()
-#     data = create_pandas("/Users/mymac/Downloads/export-2019-to-now-v4/export-2019-to-now-v4.csv")
-#     data = apply_regex(data)
-#     print(data)
-#     end = time.time()
-#     print(f"Tiempo de ejecucion: {end - start} segundos")
